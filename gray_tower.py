672bc4d38608a309efb75deb69f767fa795a86a1
antigones <juna.salviati@gmail.com>
2022-12-13 22:32:47 +0100

    m_ary Gray code generation added

--- gray_tower.py
+++ gray_tower.py
@@ -1,4 +1,5 @@
 import copy
+import itertools
 
 
 class GrayTower:
@@ -39,6 +40,27 @@ class GrayTower:
         rigs = [[] for x in range(self.n_rigs)]
         rigs[0] = list(range(self.n_disks-1, -1, -1))
         return rigs
+
+    def generate_m_ary_gray_code(self, m):
+        base = list(range(m))
+        print(base)
+
+        desc = list(range(m-1, -1, -1))
+        asc = list(range(0, m))
+
+        for k in range(m-1):
+            p = []
+            for i, b in enumerate(base):
+                if i % 2 == 0:
+                    # p += list(itertools.product([b], asc))
+                    for a in asc:
+                        p += [str(b)+str(a)]
+                else:
+                    for d in desc:
+                        p += [str(b)+str(d)]
+            base = p
+        p = [list(map(int, list(x))) for x in p]
+        return p
 
     def pretty_print(self, configurations):
         out_str = "\n"
