6c6b1c70564d880d9e628331826ae5de9ddb112b
antigones <juna.salviati@gmail.com>
2022-12-21 18:01:20 +0100

    q-learning in uncertain environment

--- hanoi_tower_qlearning.py
+++ hanoi_tower_qlearning.py
@@ -2,6 +2,7 @@ import copy
 import random as rd
 import math
 import numpy as np
+from collections import defaultdict
 
 
 class HanoiTowerQLearning:
@@ -14,7 +15,14 @@ class HanoiTowerQLearning:
         self.n_rigs = n_rigs
         self.n_disks = n_disks
         self.max_episodes = max_episodes
-        self.epsilon = epsilon
+
+        self.min_epsilon = 0.01
+        self.max_epsilon = 1.0
+        self.decay_rate = 0.01
+        self.epsilon = self.min_epsilon + \
+            (self.max_epsilon - self.min_epsilon) * np.exp(-self.decay_rate * 1)
+
+        self.total_nr_of_state = self.n_rigs ** self.n_disks
 
     def get_next_allowed_moves(self, starting_state):
         next_moves = []
@@ -36,141 +44,126 @@ class HanoiTowerQLearning:
                             next_moves.append(copied_rigs)
         return next_moves
 
-    def generate_space_states(self, from_state):
-        q = []
-        q.append(from_state)
-        space = []
-        visited = []
-        while len(q) > 0:
-            state_to_explore = q.pop(0)
-            if not state_to_explore in visited:
-                next_states = self.get_next_allowed_moves(state_to_explore)
-                visited.append(state_to_explore)
-                q += next_states
-                for state in next_states:
-                    if not state in space:
-                        space.append(state)
-        return space
+    def get_reward(self, state, visited):
+        print('** VISITED **')
+        print(state)
+        print(visited)
+        if state == self.goal_state:
+            return 100
+        if str(state) in visited:
+            print('VIS')
+            return -500
 
-    def generate_qr(self, from_state):
-        q = []
-        q.append(from_state)
-        total_nr_of_state = self.n_rigs ** self.n_disks
-        r_matrix = [[-math.inf]*total_nr_of_state]*total_nr_of_state
-        r_matrix = [[-math.inf] *
-                    total_nr_of_state for _ in range(total_nr_of_state)]
-        q_matrix = [[0]*total_nr_of_state for _ in range(total_nr_of_state)]
-        space_dict = {0: from_state}
-        index_dict = {str(from_state): 0}
-        visited = []
-        c = 1
-        while len(q) > 0:
-            state_to_explore = q.pop(0)
-            if not state_to_explore in visited:
-                visited.append(state_to_explore)
-                next_states = self.get_next_allowed_moves(state_to_explore)
-                q += next_states
-                for next_state in next_states:
-                    if str(next_state) not in index_dict.keys():
-                        space_dict[c] = next_state
-                        index_dict[str(next_state)] = c
-                        # fill reward matrix (R)
-                        c += 1
+        return -1
 
-        for start_state in index_dict.keys():
-            next_states = self.get_next_allowed_moves(eval(start_state))
-            start_state_index = index_dict[str(start_state)]
-            for next_state in next_states:
-                next_state_index = index_dict[str(next_state)]
-                if next_state == self.goal_state:
-                    r_matrix[start_state_index][next_state_index] = 100
-                else:
-                    r_matrix[start_state_index][next_state_index] = -1
+    def find_in_qsa(self, q_s_a, key):
+        res = []
+        for k in q_s_a.keys():
+            if k.startswith(key+"|"):
+                res.append(k)
+        return res
 
-        # loopback for goal state
-        goal_state_index = index_dict[str(self.goal_state)]
-        r_matrix[goal_state_index][goal_state_index] = 100
-        return space_dict, index_dict, r_matrix, q_matrix
+    def find_val_in_qsa(self, q_s_a, key):
+        res = []
+        for k in q_s_a.keys():
+            if k.startswith(key+"|"):
+                res.append(q_s_a[k])
+        return res
 
     def train(self, verbose=False):
 
-        space_dict, index_dict, r, q = self.generate_qr(
-            self.start_state)
-        possible_initial_states = list(range(len(space_dict)))
-        episode = 1
+        possible_initial_states = set()
+        possible_initial_states.add(str(self.start_state))
+
         GAMMA = self.gamma
-        GOAL_STATE = index_dict[str(self.goal_state)]
+        GOAL_STATE = self.goal_state
         ALPHA = self.alpha
         MAX_EPISODES = self.max_episodes
-        q_prec = copy.deepcopy(q)
+        #q = [[0]*self.total_nr_of_state for _ in range(self.total_nr_of_state)]
+        #q_prec = copy.deepcopy(q)
 
         done = False
         convergence_count = 0
-
-        min_epsilon = 0.01
-        max_epsilon = 1.0
-        decay_rate = 0.01
-        self.epsilon = min_epsilon + \
-            (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+        q_s_a = defaultdict(lambda: 0)
+        q_s_a_prec = q_s_a
+        episode = 1
 
         while episode < MAX_EPISODES and not done:
+            visited = []
             initial_state_for_this_episode = rd.choice(
-                possible_initial_states)
+                list(possible_initial_states))
+
             print('*** EPISODE '+str(episode)+' ***')
-            while initial_state_for_this_episode != GOAL_STATE:
-                possible_choices_for_this_state = r[initial_state_for_this_episode]
-                candidate_next_states = []
-                for i in range(len(possible_choices_for_this_state)):
-                    if possible_choices_for_this_state[i] != -math.inf:
-                        candidate_next_states.append(i)
+            print('*** POSSIBLE INITIAL STATES FOR THIS EPISODE ***')
+            print(len(possible_initial_states))
+            while eval(initial_state_for_this_episode) != GOAL_STATE:
+                # print(initial_state_for_this_episode)
+                possible_next_states_for_this_state = self.get_next_allowed_moves(
+                    eval(initial_state_for_this_episode))
+                # print(possible_next_states_for_this_state)
+                rewards_for_actions = {}
+                q_s1_a = []
+                for next_state in possible_next_states_for_this_state:
+                    rewards_for_actions[str(next_state)
+                                        ] = self.get_reward(next_state, visited)
+                    q_s1_a.append(
+                        q_s_a[initial_state_for_this_episode+"|"+str(next_state)])
+                    possible_initial_states.add(str(next_state))
 
-                print(initial_state_for_this_episode)
-                print(candidate_next_states)
-
+                #print('** R FOR ACTIONS **')
+                # print(rewards_for_actions)
+                #print('** QS1A **')
+                # print(q_s1_a)
+                # q_s_list = self.find_in_qsa(
+                #    q_s_a, initial_state_for_this_episode)
+                q_s_list = {x: q_s_a[x] for x in q_s_a.keys() if x.startswith(
+                    initial_state_for_this_episode+"|")}
+                #print('** QSLIST **')
+                # print(q_s_list)
                 e = rd.uniform(0, 1)
                 if e < self.epsilon:
-                    next_state = rd.choice(candidate_next_states)
+                    chosen_next_state = str(rd.choice(
+                        possible_next_states_for_this_state))
                 else:
-                    next_state = np.argmax(r[initial_state_for_this_episode])
+                    m = max(
+                        q_s_list, key=q_s_list.get)
+                    chosen_next_state = m.split("|")[1]
+                visited.append(chosen_next_state)
+                q_s_a[initial_state_for_this_episode+"|"+chosen_next_state] += ALPHA * (
+                    rewards_for_actions[chosen_next_state] + GAMMA * max(q_s1_a) - q_s_a[initial_state_for_this_episode+"|"+chosen_next_state])
 
-                # naive way to update
-                # q[initial_state_for_this_episode][next_state] = r[initial_state_for_this_episode][next_state] + \
-                #    GAMMA * max(q[next_state])
+                # print('**QSA**')
+                # print(q_s_a)
+                initial_state_for_this_episode = chosen_next_state
 
-                ## another way to update ##
-                q_target = r[initial_state_for_this_episode][next_state] + \
-                    GAMMA * max(q[next_state])
-                q_delta = q_target - \
-                    q[initial_state_for_this_episode][next_state]
-                q[initial_state_for_this_episode][next_state] = q[initial_state_for_this_episode][next_state] + \
-                    ALPHA*(q_delta)
-                ## end of another way to update ##
-                initial_state_for_this_episode = next_state
-
-            if np.allclose(q, q_prec) and sum(sum(x) for x in q) > 0:
+            if q_s_a == q_s_a_prec:
                 if convergence_count > int(10):
-                    print('CONVERGE!')
+                    print('** CONVERGED **')
 
                     done = True
                     break
                 else:
                     convergence_count += 1
             else:
-                q_prec = copy.deepcopy(q)
+                q_s_a_prec = q_s_a
                 convergence_count = 0
 
             # epsilon update
 
-            self.epsilon = min_epsilon + \
-                (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+            self.epsilon = self.min_epsilon + \
+                (self.max_epsilon - self.min_epsilon) * \
+                np.exp(-self.decay_rate * episode)
             episode += 1
 
-        next_state = 0
-        print(space_dict[next_state])
+        print(q_s_a)
+
         c = 0
-        while next_state != GOAL_STATE:
-            edges = q[next_state]
-            (m, i) = max((v, i) for i, v in enumerate(edges))
-            print(space_dict[i])
-            next_state = i
+        next_state = str(self.start_state)
+        while next_state != str(self.goal_state) and c <= 10:
+            candidate_next_list = {x: q_s_a[x] for x in q_s_a.keys() if x.startswith(
+                next_state+"|")}
+            m = max(
+                candidate_next_list, key=candidate_next_list.get)
+            next_state = m.split("|")[1]
+            print(next_state)
             c += 1
